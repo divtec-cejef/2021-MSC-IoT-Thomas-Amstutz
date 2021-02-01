76f2b4408156a663b1a7cb5e7fd53950225cf9fa
Thomas Amstutz <54527950+ThomasAmstutz@users.noreply.github.com>
2021-02-01 16:25:00 +0100

    Ajout du code de test du capteur DHT

---
+++ src/arduino/other/sensors/DHTtester.ino
@@ -0,0 +1,38 @@
+#include <DHT_U.h>
+#include <DHT.h>
+#include <Adafruit_Sensor.h>
+
+#define DHTPIN 2
+
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%\t  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C"));
+}
